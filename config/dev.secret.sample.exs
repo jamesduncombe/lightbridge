1dddd613a90c5c74a44a9b755d99eba4f2c820b1
James Duncombe <james@jamesduncombe.com>
2020-10-07 19:53:04 +0100

    Add default configs

---
+++ config/dev.secret.sample.exs
@@ -0,0 +1,12 @@
+import Config
+
+config :lightbridge,
+  # IP of the HS100
+  hs100_ip: {0, 0, 0, 0},
+  # MQTT hostname
+  mqtt_host: "",
+  # Port of the MQTT server
+  mqtt_port: 0,
+  # Credentials for the MQTT server
+  mqtt_username: "",
+  mqtt_password: ""
