06eedada985ff2ad437465f5d734fccb81872cef
James Duncombe <james@jamesduncombe.com>
2020-10-08 16:52:40 +0100

    Refactor hs100 module

--- lib/lightbridge/hs100.ex
+++ lib/lightbridge/hs100.ex
@@ -55,8 +55,14 @@ defmodule Lightbridge.Hs100 do
   end
 
   # TODO: Add proper return from a relay switch
+  @doc """
+  Sends the `cmd` to the HS100/110.
+  """
+  @spec send_cmd(cmd :: String.t()) :: binary()
   def send_cmd(cmd) do
-    encrypted = encrypt(cmd)
+    encrypted =
+      cmd
+      |> encrypt()
 
     {:ok, sock} = :gen_tcp.connect(hs100_ip(), 9999, [:binary, {:packet, 0}, {:active, false}])
 
@@ -74,40 +80,37 @@ defmodule Lightbridge.Hs100 do
   @spec encrypt(cmd :: String.t()) :: list()
   def encrypt(cmd) do
     # Encode the length as a uint32
-    length_of_payload = byte_size(cmd)
-    ciphertext = [0, 0, 0, length_of_payload]
+    ciphertext = [0, 0, 0, byte_size(cmd)]
 
     # Build the payload
-    payload = do_payload(cmd, _accm = [], @encryption_key)
+    payload = do_encrypt_payload(cmd, _accm = [], @encryption_key)
 
     # Append the encrypted payload to the ciphertext
     ciphertext ++ payload
   end
 
-  def do_payload(<<>>, accm, _key), do: accm
+  defp do_encrypt_payload(<<>>, accm, _key), do: accm
 
-  def do_payload(<<byte, rest::binary>>, accm, key) do
+  defp do_encrypt_payload(<<byte, rest::binary>>, accm, key) do
     slam = bxor(key, byte)
-    do_payload(rest, accm ++ [slam], slam)
+    do_encrypt_payload(rest, accm ++ [slam], slam)
   end
 
   @doc """
-  Handles decrypting commands to HS100.
+  Handles decrypting commands from HS100.
   """
   @spec decrypt(ciphertext :: binary()) :: String.t()
   def decrypt(ciphertext) do
+    # Ignore the first 4 bytes as that's the length of the ciphertext (uint32)
     <<_, _, _, _, rest::binary>> = ciphertext
 
-    # Build the payload
-    payload = do_decrypt_payload(rest, _accm = [], @encryption_key)
-
-    # Append the encrypted payload to the ciphertext
-    payload
+    # Recursively build the payload
+    do_decrypt_payload(rest, _accm = [], @encryption_key)
   end
 
-  def do_decrypt_payload(<<>>, accm, _key), do: accm
+  defp do_decrypt_payload(<<>>, accm, _key), do: accm
 
-  def do_decrypt_payload(<<byte, rest::binary>>, accm, key) do
+  defp do_decrypt_payload(<<byte, rest::binary>>, accm, key) do
     next_key = byte
     slam = bxor(key, byte)
     do_decrypt_payload(rest, accm ++ [slam], next_key)
