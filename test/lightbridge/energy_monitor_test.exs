2fb304ae228d2cdcc52e202a933ee3a1e0c43b3b
James Duncombe <james@jamesduncombe.com>
2020-10-27 11:56:15 +0000

    Update test

--- test/lightbridge/energy_monitor_test.exs
+++ test/lightbridge/energy_monitor_test.exs
@@ -6,7 +6,7 @@ defmodule Lightbridge.EnergyMonitorTest do
   describe "parse_energy_stats/1" do
     test "parses energy stats from the socket into a flattened map" do
       raw_json =
-        "{\"emeter\":{\"get_realtime\":{\"voltage_mv\":246486,\"current_ma\":46,\"power_mw\":5715,\"total_wh\":1958,\"err_code\":0}}}"
+        ~s({"emeter":{"get_realtime":{"voltage_mv":246486,"current_ma":46,"power_mw":5715,"total_wh":1958,"err_code":0}}})
 
       parsed_stats =
         raw_json
